b4ce0f26bb87204d01d0ef9d5db56c3ca1060bc0
vinic <vinic@LAPTOP-4IQ0C9JM>
2019-12-06 02:06:23 -0300

    exercicios 1 a 3

---
+++ ExercicioArray/src/br/com/ultracar/ExercicioArray.java
@@ -0,0 +1,44 @@
+package br.com.ultracar;
+
+public class ExercicioArray {
+	
+	/* 1
+	public static void main(String[] args) {
+		int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(i);
+		}
+	}
+	*/
+	
+	/* 2
+	public static void main(String[] args) {
+		int[] array = new int[]{1, 2, 3, 4, 5};
+		
+		int soma = 0;
+		int media;
+		
+		for (int i = 0; i < array.length; i++) {
+			soma = soma + i;
+		}
+		
+		media = soma/array.length;
+		System.out.println(media);
+	}
+	*/
+	
+	/* 3
+	public static void main(String[] args) {
+		int [] array = new int[] {1,2,3,4,5,6,7,8,9};
+		
+		int mult = array[0];
+		
+		for(int i = 1;i< array.length;i++) {
+			mult = mult * array[i];
+		}
+		
+		System.out.println(mult);
+	}
+	*/
+}
